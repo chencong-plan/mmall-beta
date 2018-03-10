6b32d5851ddf55d1584e1d7dba2754c09aa1c96a
chencong-plan <1042738887@qq.com>
2018-03-10 23:28:05 +0800

    完善RedisPool工具类(包含main测试方法) PropertiesUtil方法当中新增获得boolean和int的方法及其重载

--- src/main/java/com/mmall/common/RedisPool.java
+++ src/main/java/com/mmall/common/RedisPool.java
@@ -1,6 +1,9 @@
 package com.mmall.common;
 
+import com.mmall.util.PropertiesUtil;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Jedis连接Redis的连接池
@@ -18,30 +21,110 @@ public class RedisPool {
     /**
      * 最大连接数
      */
-    private static Integer maxTotal = 0;
+    private static Integer maxTotal = PropertiesUtil.getIntegerProperty("redis.max.total", 20);
 
     /**
      * 在jedisPool中最大idle状态(空闲)
      */
-    private static Integer maxIdle = 0;
+    private static Integer maxIdle = PropertiesUtil.getIntegerProperty("redis.max.idle", 10);
 
     /**
      * 在jedisPool当中最小的idle状态(空闲)
      */
-    private static Integer minIdle = 0;
+    private static Integer minIdle = PropertiesUtil.getIntegerProperty("redis.min.idle", 2);
 
     /**
      * 在Borrow一个jedis实例的时候是否进行验证操作。
      * 如果赋值为true，则拿到的jedis是可用的
      */
-    private static Boolean testOnBorrow = true;
+    private static Boolean testOnBorrow = PropertiesUtil.getBooleanProperty("redis.test.borrow", true);
 
 
     /**
      * 在return一个jedis实例时候，是否要进行测试，
      * 如果赋值为true时，则放回的jedis实例为可用的
      */
-    private static Boolean testOnReturn = true;
+    private static Boolean testOnReturn = PropertiesUtil.getBooleanProperty("redis.test.return", true);
+    /**
+     * 获取RedisIP
+     */
+    private static String redisIp = PropertiesUtil.getProperty("redis.ip");
+
+    /**
+     * 获取RedisPort
+     */
+    private static Integer redisPort = PropertiesUtil.getIntegerProperty("redis.port");
+
+    /**
+     * 初始化JedisPoolConfig连接池
+     */
+    private static void initPool() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxTotal);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+
+        /*
+         * 连接耗尽时是否阻塞，false则会抛出异常，true阻塞直到超时，默认为true
+         */
+        config.setBlockWhenExhausted(true);
+
+        pool = new JedisPool(config, redisIp, redisPort, 1000 * 2);
+    }
+
+    /*
+     * 初始化Jedis连接池
+     */
+    static {
+        initPool();
+    }
+
+    /**
+     * 获取jedis连接
+     *
+     * @return jedis
+     */
+    public static Jedis getJedis() {
+        return pool.getResource();
+    }
+
+
+    /**
+     * 放回jedis
+     *
+     * @param jedis jedis
+     */
+    public static void returnBrokenResource(Jedis jedis) {
+        pool.returnBrokenResource(jedis);
+    }
+
+    /**
+     * 放回jedis
+     *
+     * @param jedis jedis
+     */
+    public static void returnResource(Jedis jedis) {
+        pool.returnResource(jedis);
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = pool.getResource();
+        jedis.set("chencongKey","chencongValue");
+        returnResource(jedis);
+
+        /*获取值*/
+        System.out.println(jedis.get("chencongKey"));
+
+        /*
+         * 临时调用，销毁连接池中所有连接
+         */
+        pool.destroy();
+
+        System.out.println("program is end");
+    }
 
 
 }
--- src/main/java/com/mmall/util/PropertiesUtil.java
+++ src/main/java/com/mmall/util/PropertiesUtil.java
@@ -9,41 +9,148 @@ import java.io.InputStreamReader;
 import java.util.Properties;
 
 /**
- * Created by geely
+ * 读取properties配置文件的工具类<br>
+ * 2018年3月10日21:44:34 新增获取Boolean int类型的方法及其重载方法
+ *
+ * @author chencong
  */
 public class PropertiesUtil {
 
+    /**
+     * 读取日志
+     */
     private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
 
+    /**
+     * properties属性
+     */
     private static Properties props;
 
+    /*
+     * 静态代码块读取制定配置文件
+     */
     static {
-        String fileName = "src/main/resources.beta/mmall.properties";
+        /*
+         * 应为使用了maven环境隔离，编译之后都存在于src下面，因此这里使用指明读取的是哪一个配置文件就行了<br>
+         * 切记不要使用这种：src/resources.dev/mmall.properties 会出现异常
+         */
+        String fileName = "mmall.properties";
         props = new Properties();
         try {
-            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
+            props.load(
+                    new InputStreamReader(
+                            PropertiesUtil.class
+                                    .getClassLoader().
+                                    getResourceAsStream(fileName), "UTF-8"));
         } catch (IOException e) {
-            logger.error("配置文件读取异常",e);
+            logger.error("配置文件读取异常", e);
         }
     }
 
-    public static String getProperty(String key){
+    /**
+     * 获取配置文件当中key所对应值
+     *
+     * @param key key
+     * @return 返回key所对应的值，存在则返回，不存在则返回null
+     */
+    public static String getProperty(String key) {
         String value = props.getProperty(key.trim());
-        if(StringUtils.isBlank(value)){
+        if (StringUtils.isBlank(value)) {
             return null;
         }
         return value.trim();
     }
 
-    public static String getProperty(String key,String defaultValue){
-
+    /**
+     * 获取配置文件当中key所对应的值，同时给予默认值。<br>
+     * 当key所对应的值不存在时，返回参数当中给予的默认值
+     *
+     * @param key          key
+     * @param defaultValue 默认值
+     * @return 返回key对应值，不存在或者为null时返回默认值defaultValue
+     */
+    public static String getProperty(String key, String defaultValue) {
         String value = props.getProperty(key.trim());
-        if(StringUtils.isBlank(value)){
+        if (StringUtils.isBlank(value)) {
             value = defaultValue;
         }
         return value.trim();
     }
 
+    /**
+     * 获取配置文件当中key所对应值，存在则进行数字转换。将其返回
+     *
+     * @param key key
+     * @return 返回key对应的值，不存在或发生NumberFormatException则返回null
+     */
+    public static Integer getIntegerProperty(String key) {
+        String value = props.getProperty(key.trim());
+        Integer result;
+        try {
+            result = Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            logger.info("参数转换异常：" + e.getMessage());
+            return null;
+        }
+        return result;
+    }
 
+    /**
+     * 获取配置文件当中key所对应值，存在则进行数字转换。将其返回
+     *
+     * @param key          key
+     * @param defaultValue 默认值
+     * @return 返回key对应的值，不存在时或发生NumberFormatException则返回defaultValue
+     */
+    public static Integer getIntegerProperty(String key, Integer defaultValue) {
+        String value = props.getProperty(key);
+        Integer result;
+        if (StringUtils.isBlank(value)) {
+            result = defaultValue;
+            return  result;
+        }
+        try {
+            result = Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            logger.info("参数转换异常：" + e.getMessage());
+            result = defaultValue;
+        }
+        return result;
+    }
 
+    /**
+     * 获取配置文件当中key所对应值<br>
+     * 如果其值为true则返回true，false或其他值则返回false,为空时返回null
+     *
+     * @param key key
+     * @return key对应的值，值为true时返回true，false或其他值则返回false
+     */
+    public static Boolean getBooleanProperty(String key) {
+        String value = props.getProperty(key.trim()).trim();
+        Boolean result;
+        if (StringUtils.isBlank(value)) {
+            return null;
+        }
+        result = "true".equals(value);
+        return result;
+    }
+
+    /**
+     * 获取配置文件当中key所对应值<br>
+     * 如果其值为true则返回true，false或其他值则返回false，为null时返回defaultValue
+     *
+     * @param key          key
+     * @param defaultValue 默认值
+     * @return key对应的值，值为true时返回true，false或其他值则返回false
+     */
+    public static Boolean getBooleanProperty(String key, Boolean defaultValue) {
+        String value = props.getProperty(key.trim()).trim();
+        Boolean result;
+        if (StringUtils.isBlank(value)) {
+            result = defaultValue;
+            return  result;
+        }
+        result = "true".equals(value);
+        return result;
+    }
 }
