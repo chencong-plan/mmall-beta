a0b8020eec411d170e216f49a3d733bc8097473e
chencong-plan <1042738887@qq.com>
2018-03-13 22:43:00 +0800

    用户登录将用户Json存入redis,同时将loginToken存入Cookie，用户json存入redis,登出操作从cookie当中删除loginToken信息，同时删除redis当中loginToken对应的value

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -8,6 +8,7 @@ import com.mmall.service.IUserService;
 import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +40,6 @@ public class UserController {
      * @param password            password
      * @param session             session
      * @param httpServletResponse httpServletResponse
-     * @param httpServletRequest  httpServletRequest
      * @return 返回json
      */
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
@@ -47,8 +47,7 @@ public class UserController {
     public ServerResponse<User> login(String username,
                                       String password,
                                       HttpSession session,
-                                      HttpServletResponse httpServletResponse,
-                                      HttpServletRequest httpServletRequest) {
+                                      HttpServletResponse httpServletResponse) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             /*此时为单节点tomcat，在这里已经将session存储在redis 当中了*/
@@ -58,17 +57,31 @@ public class UserController {
             /*sessionId 存储在cookie当中*/
             CookieUtil.writeLoginToken(httpServletResponse, session.getId());
             /*从cookie当中独处cookie*/
-           // CookieUtil.readLoginToken(httpServletRequest);
+            // CookieUtil.readLoginToken(httpServletRequest);
 
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()), Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
     }
 
+
+    /**
+     * 登出操作<br>
+     * 首先从cookie当中获取到loginToken<br>
+     * 然后再根据loginToken到cookie当中删除(设置maxAge(0))<br>
+     * 最后从Redis当中删除loginToken对应的User序列化Json
+     *
+     * @param request  request
+     * @param response response
+     * @return 返回服务器响应
+     */
     @RequestMapping(value = "logout.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> logout(HttpSession session) {
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        /*session.removeAttribute(Const.CURRENT_USER);*/
+        String loginToken = CookieUtil.readLoginToken(request);
+        CookieUtil.delLoginToken(request, response);
+        RedisPoolUtil.del(loginToken);
         return ServerResponse.createBySuccess();
     }
 
@@ -86,10 +99,29 @@ public class UserController {
     }
 
 
+    /**
+     * get_user_info获取用户信息<br>
+     * 首先判断是否存在loginToken，如果不存在则直接返回"用户未登录，无法获取当前用户的信息"<br>
+     * 否则，将loginToken作为key 从Redis当中获取到用户User的Json数据 <br>
+     * 然后将获取到的JSON字符串转成User对象<br>
+     * 判断User对象是否为空，为null返回未登录，否则返回相应用户信息
+     *
+     * @param httpServletRequest request
+     * @return 返回请求
+     */
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpServletRequest httpServletRequest) {
+        /*User user = (User) session.getAttribute(Const.CURRENT_USER);*/
+        /*不在从session当中获取用户，而是从cookie token当中*/
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        /*无法从cookie当中获取到loginToken，直接返回用户未登录*/
+        if (StringUtils.isEmpty(loginToken)) {
+            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+        }
+        /*拿到loginToken*/
+        String userJsonStr = RedisPoolUtil.get(loginToken);
+        User user = JsonUtil.String2Obj(userJsonStr, User.class);
         if (user != null) {
             return ServerResponse.createBySuccess(user);
         }
